4df709294aadcd41dd1760cd298c48a7be4da798
Nahum Sá <nahumsaa@gmail.com>
2022-09-06 00:04:40 -0300

    add experiment tracking

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -1,39 +1,72 @@
 import logging
-
 from pathlib import Path
-from typing import List
-from sklearn.pipeline import Pipeline
+from typing import List, Tuple
+
+import mlflow
+import numpy as np
+import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.pipeline import Pipeline
 
-from logger import init_logger
 from etl.extract import load_data
 from etl.transform import preprocess_data
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("linear-regression")
 
-
-init_logger(Path("..", "logs", "linear_regression.log"))
-logger = logging.getLogger("main")
 
 def pipeline_steps() -> Pipeline:
     steps = [
-            ("dictvectorizer", DictVectorizer()),
-            ("linearregression", LinearRegression())
-            ]
-    
+        ("dictvectorizer", DictVectorizer()),
+        ("linearregression", LinearRegression()),
+    ]
+
     return Pipeline(steps=steps)
 
 
-def train(year: int, month: int, cat_columns: List[str]=["PULocationID", "DOLocationID"], target="duration") -> Pipeline:
-    features = []
-    df = load_data(year=year, month=month)
+def process_model_data(df: pd.DataFrame, cat_columns: List[str]) -> np.array:
     process_df = preprocess_data(df)
+    return process_df, process_df[cat_columns].astype(str).to_dict(orient="records")
+
+
+def eval_metrics(actual: List[float], pred: List[float]) -> Tuple[float, float, float]:
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+def train_and_valid(
+    year: int,
+    month: int,
+    cat_columns: List[str] = ["PULocationID", "DOLocationID"],
+    target="duration",
+) -> Pipeline:
+    
+    features = []
+
+    df_train = load_data(year=year, month=month)
+    df_valid = load_data(year=year, month=month + 1)
+
+    mlflow.autolog()
     pipe = pipeline_steps()
-    
-    train_dicts = process_df[cat_columns].astype(str).to_dict(orient="records")
+
+    process_df_train, train_dicts = process_model_data(df_train, cat_columns=cat_columns)
     features.extend(cat_columns)
-    
-    y = process_df[target].values
+
+    y = process_df_train[target].values
     pipe.fit(train_dicts, y)
-    
-    return pipe
+
+    process_df_valid, valid_dicts = process_model_data(df_valid, cat_columns=cat_columns)
+    y_valid = process_df_valid[target].values
+    valid_prediction = pipe.predict(valid_dicts)
+
+    rmse, mae, r2 = eval_metrics(y_valid, valid_prediction)
+
+    mlflow.log_metric("valid_rmse", rmse)
+    mlflow.log_metric("valid_r2", r2)
+    mlflow.log_metric("valid_mae", mae)
+
+    return pipe, features
